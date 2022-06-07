00d55f7cd75f43388db7943bf901ca2ed854dd8f
Jaroslav <garasym@gmail.com>
2022-06-07 10:43:59 +0300

    Maven Project. Template

---
+++ src/test/java/com/softserve/edu/My.java
@@ -0,0 +1,15 @@
+package com.softserve.edu;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class My {
+
+    @Test
+    public void testMy() {
+        // From Maven
+        System.out.println("My done");
+        //
+        Assert.assertEquals(9, 4 + 5);
+    }
+}
