396b8d796253527055b11a811a6d8f1a2ffb88c1
Jaroslav <garasym@gmail.com>
2022-06-09 13:38:43 +0300

    hashCode and equals

--- src/main/java/com/softserve/edu03/AppArr.java
+++ src/main/java/com/softserve/edu03/AppArr.java
@@ -57,14 +57,29 @@ public class AppArr {
         System.out.println("isEquals = " + isEquals);
         System.out.println("Arrays.equals(arr, arr0) = " + Arrays.equals(arr, arr0));
         */
+        /*
+        Person p1= new Person(2,"Ivan");
+        Person p2= new Person(2,"Ivan");
         //
+        System.out.println("p1 = " + p1.toString());
+        System.out.println("p2 = " + p2);
+        System.out.println("(p1 == p2) = " + (p1 == p2));
+        System.out.println("p1.equals(p2) = " + p1.equals(p2));
+        System.out.println("p1 = " + p1.hashCode());
+        System.out.println("p2 = " + p2.hashCode());
+        */
+        //
+        //  /*
         Person[] arr = { new Person(2,"Ivan"), new Person(8,"Petro") };
         Person[] arr0 = { new Person(2,"Ivan"), new Person(8,"Petro") };
         //
         boolean isEquals = (arr0.length == arr.length);
         for (int i = 0; isEquals && i < arr.length; i++) {
-            isEquals = arr[i] == arr0[i];
+            //isEquals = arr[i] == arr0[i];
+            isEquals = arr[i].equals(arr0[i]);
         }
         System.out.println("isEquals = " + isEquals);
+        System.out.println("Arrays.equals(arr, arr0) = " + Arrays.equals(arr, arr0));
+        // */
     }
 }
--- src/main/java/com/softserve/edu03/Person.java
+++ src/main/java/com/softserve/edu03/Person.java
@@ -1,5 +1,7 @@
 package com.softserve.edu03;
 
+import java.util.Objects;
+
 public class Person {
     private int id;
     private String name;
@@ -24,4 +26,50 @@ public class Person {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("\tpublic boolean equals(Object obj) running ...");
+        /*
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Person person = (Person) obj;
+        return id == person.id && Objects.equals(name, person.name);
+        */
+        if (this == obj) {
+            return true;
+        }
+        if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+        Person other = (Person) obj;
+        if ((id != other.id)
+                || ((name == null) && (other.name != null))
+                || ((name != null) && (other.name == null))) {
+            return false;
+        }
+        if ((name == null) && (other.name == null)) {
+            return true;
+        }
+        return name.equals(other.name);
+    }
+
+    @Override
+    public int hashCode() {
+        //return Objects.hash(id, name);
+        final int prime = 31; // 2^5-1
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                "]";
+    }
 }
+
