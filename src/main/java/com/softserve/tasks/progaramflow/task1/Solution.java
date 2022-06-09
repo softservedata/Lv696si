be74414708092b74de5c25f81b884fd09cc4ea7c
Rostyslav <rostylka96@gmail.com>
2022-06-09 10:23:32 +0300

    project

--- src/main/java/com/softserve/tasks/progaramflow/task1/Solution.java
+++ src/main/java/com/softserve/tasks/progaramflow/task1/Solution.java
@@ -2,6 +2,14 @@ package com.softserve.tasks.progaramflow.task1;
 
 public class Solution {
     public boolean isPalindrome(int x) {
-        return false;
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int result = 0;
+        while(x > result) {
+            result = result * 10 + x % 10;
+            x /= 10;
+        }
+        return x == result || x == result/10;
     }
 }
