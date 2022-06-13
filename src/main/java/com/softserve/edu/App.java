42928b0e668c94b7611bf647f72acaa12525d654
asya1fahradova <asya1fahradova@gmail.com>
2022-06-13 18:38:26 +0300

    task 1,2

--- src/main/java/com/softserve/edu/App.java
+++ src/main/java/com/softserve/edu/App.java
@@ -11,3 +11,42 @@ public class App
         System.out.println( "Hello World!" );
     }
 }
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0;
+        int ori=x;
+        if (x<0 || (x!=0 && x%10==0))
+            return false;
+        while(x!=0){
+            rev=rev*10+x%10;
+            x=x/10;
+        }
+        return ori==rev;
+    }
+}
+
+
+class Solution {
+    public int reverse(int x) {
+        // INT_MAX: 2147483647
+        // INT_MIN: -2147483648
+        int result = 0;
+
+        while (x != 0) {
+            int mod = x % 10;
+
+            if (result > 214748364 || (result == 214748364 && mod > 7)) {
+                return 0;
+            }
+            if (result < -214748364 || (result == -214748364 && mod == -9)) {
+                return 0;
+            }
+
+            result = 10 * result + mod;
+            x /= 10;
+        }
+
+        return result;
+    }
+};
