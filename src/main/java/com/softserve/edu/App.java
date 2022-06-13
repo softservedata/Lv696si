2af4646f3ff04dc66a5b95a8d96b1c3304a23295
Rostyslav <rostylka96@gmail.com>
2022-06-13 13:57:47 +0300

    Task1

--- src/main/java/com/softserve/edu/App.java
+++ src/main/java/com/softserve/edu/App.java
@@ -9,7 +9,11 @@ import java.util.Comparator;
 public class App {
     public static void main(String[] args) {
         int[][] arr = {
-                {1, 2 , 3},
+                {1, 2, 3},
+                        { 1},
+                {},
+                        {3, 2},
+                        {3, 3, 5}
                 };
         for (int i = 0; i < arr.length; i++) {
             System.out.println(Arrays.toString(arr[i]));
