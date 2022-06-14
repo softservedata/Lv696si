89af7542edbc773a8474b759f48f287848162323
Rostyslav <rostylka96@gmail.com>
2022-06-14 17:00:29 +0300

    Task3

--- src/main/java/com/softserve/edu/App.java
+++ src/main/java/com/softserve/edu/App.java
@@ -10,11 +10,11 @@ public class App {
     public static void main(String[] args) {
         int[][] arr = {
                 {1, 2, 3},
-                        { 1},
+                {1},
                 {},
-                        {3, 2},
-                        {3, 3, 5}
-                };
+                {3, 2},
+                {3, 3, 5}
+        };
         for (int i = 0; i < arr.length; i++) {
             System.out.println(Arrays.toString(arr[i]));
         }
@@ -27,39 +27,34 @@ public class App {
 
     // Code
     public static int[][] arrSort(int[][] arr) {
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1.length == 0 && o2.length != 0)
-                    return -1;
-                if (o1.length != 0 && o2.length == 0)
-                    return 1;
-                if (o1.length == 0 && o2.length == 0)
-                    return 0;
-                if (o1[0] < o2[0]) {
-                    return 1;
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1.length == 0 && o2.length != 0)
+                return 1;
+            if (o1.length != 0 && o2.length == 0)
+                return -1;
+            if (o1.length == 0 && o2.length == 0)
+                return 0;
+            if (o1[0] < o2[0]) {
+                return 1;
+            } else {
+                if (o1[0] == o2[0]) {
+                    if (o1.length > 1 && o2.length > 1) {
+                        if (o1[1] >= o2[1]) {
+                            return 1;
+                        } else {
+                            return -1;
+                        }
+                    } else {
+                        if (o1.length > 1 && o2.length <= 1) {
+                            return 1;
+                        }
+                        if (o1.length <= 1 && o2.length > 1) {
+                            return -1;
+                        }
+                        return 0;
+                    }
                 } else {
-                    if (o1[0] == o2[0]) {
-                        if (o1.length > 1 && o2.length > 1) {
-                            if (o1[1] >= o2[1]) {
-                                return 1;
-                            } else {
-                                return -1;
-                            }
-                        }
-                        else {
-                            if (o1.length > 1 && o2.length < 1) {
-                                return -1;
-                            }
-                            if (o1.length < 1 && o2.length > 1) {
-                                return 1;
-                            }
-                            return 0;
-                        }
-                    }
-                    else {
-                        return -1;
-                    }
+                    return -1;
                 }
             }
         });
--- src/main/java/com/softserve/edu/App1.java
+++ src/main/java/com/softserve/edu/App1.java
@@ -2,20 +2,23 @@ package com.softserve.edu;
 
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 
 public class App1 {
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("aa", null, "dd", "cc");
-        //List<String> list = new ArrayList<>();
+        //List<String> list = Arrays.asList("aa", null, "cc");
+        List<String> list = new ArrayList<>();
         Map<String, String> map = new HashMap<>();
-        map.put("1", "aa");
+        /*map.put("1", "aa");
         map.put("2", "dd");
         map.put("3", "cc");
         map.put("4", null);
+        map.put(null, null);*/
         System.out.println(listMapCompare(list, map));
+        System.out.println(listMapCompare1(list, map));
+        System.out.println(listMapCompare1(null, null));
+
     }
 
     public static boolean listMapCompare(List<String> list, Map<String, String> map) {
@@ -45,5 +48,19 @@ public class App1 {
             }
         }
         return result;
+    }
+    public static boolean listMapCompare1(List<String> list, Map<String, String> map) {
+        if (list == null && map == null) {
+            return true;
+        }
+        HashSet<String> set1 = new HashSet<>();
+        HashSet<String> set2 = new HashSet<>();
+        for (String str : list) {
+            set1.add(str);
+        }
+        for (Map.Entry<String, String> entrySet : map.entrySet()) {
+            set2.add(entrySet.getValue());
+        }
+        return set2.containsAll(set1);
     }
 }
