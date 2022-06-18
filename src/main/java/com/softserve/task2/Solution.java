90e3f5b3e8608da4ddb8f642cc214c550b890aed
bebebe <hahaha@gmail.com>
2022-06-18 19:01:08 +0300

    Task2

--- src/main/java/com/softserve/task2/Solution.java
+++ src/main/java/com/softserve/task2/Solution.java
@@ -1,8 +1,56 @@
 package com.softserve.task2;
-
 class Solution {
+    public
+    boolean isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while(x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber/10;
+    }
+    public
+    int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+    static int divide(int dividend, int divisor)
+    {
+        int sign
+                = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+        int quotient = 0;
+
+        while (dividend >= divisor) {
+            dividend -= divisor;
+            ++quotient;
+        }
+        if (sign == -1)
+            quotient = -quotient;
+
+        return quotient;
+    }
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 3;
+
+        System.out.println(divide(a, b));
+    }
     public int mySqrt(int x) {
         long low = 0, high = x, res = 0;
+
         while (low <= high) {
             long mid = (low + high) / 2;
             long mulMid = mid * mid;
@@ -19,37 +67,4 @@ class Solution {
         return ((int) res);
     }
 
-    public int reverse(int x) {
-        long res = 0;
-        while (x != 0) {
-            res = res * 10 + x % 10;
-            x = x / 10;
-        }
-        if (res < Integer.MIN_VALUE || res > Integer.MAX_VALUE) {
-            return 0;
-        } else {
-            return (int) res;
-        }
 
-        public int mySqrt ( int x){
-            long low = 0, high = x, res = 0;
-
-
-            while (low <= high) {
-                long mid = (low + high) / 2;
-                long mulMid = mid * mid;
-
-
-                if (mulMid == x)
-                    return ((int) mid);
-                if (mulMid > x) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                    res = mid;
-                }
-            }
-            return ((int) res);
-        }
-    }
-}
