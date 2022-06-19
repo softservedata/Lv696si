7110bc6c35d0a44be04bbf204fc311b067799e84
Rostylka <rostylka96@gmail.com>
2022-06-19 11:55:27 +0300

    Task 2.

--- src/main/java/com/softserve/task2/Solution.java
+++ src/main/java/com/softserve/task2/Solution.java
@@ -1,4 +1,5 @@
 package com.softserve.task2;
+
 class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0 || (x % 10 == 0 && x != 0)) {
@@ -13,12 +14,13 @@ class Solution {
     }
 
     public int reverse(int x) {
+
         int rev = 0;
         while (x != 0) {
             int pop = x % 10;
             x /= 10;
-            if (rev > Integer.INT_MAX / 10 || (rev == Integer.INT_MAX / 10 && pop > 7)) return 0;
-            if (rev < Integer.INT_MIN / 10 || (rev == Integer.INT_MIN / 10 && pop < -8)) return 0;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
             rev = rev * 10 + pop;
         }
         return rev;
