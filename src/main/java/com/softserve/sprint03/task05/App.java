47bced4a893d8545b3eb913cd3006e7e93a26b71
Jaroslav <garasym@gmail.com>
2022-06-20 15:47:14 +0300

    Sprint 03 tasks 01 and 05; Sprint 08 task 06

---
+++ src/main/java/com/softserve/sprint03/task05/App.java
@@ -0,0 +1,17 @@
+package com.softserve.sprint03.task05;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class App {
+    public static void main(String[] args) {
+        /*
+        Set<Integer> s1 = new HashSet<>(Arrays.asList(1, 3, 5, 7, 9));
+        Set<Integer> s2 = new HashSet<>(Arrays.asList(2, 4, 6, 8, 3, 7));
+        s1.retainAll(s2);
+        System.out.println("s1 = " + s1);
+        */
+        //
+    }
+}
