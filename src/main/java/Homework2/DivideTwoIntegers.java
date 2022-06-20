45a5723328d3cfc2bf80844e9d62676d2ec6537f
Oleh Rudenko <oleh.rudenko96@gmail.com>
2022-06-20 12:12:59 +0300

    NEW

--- src/main/java/Homework2/DivideTwoIntegers.java
+++ src/main/java/Homework2/DivideTwoIntegers.java
@@ -1,6 +1,27 @@
 package Homework2;
 
 public class DivideTwoIntegers {
+        public int divide(int dividend, int divisor)
+        {
+            if(dividend==-2147483648 && divisor==-2147483648)
+            {
+                return 1;
+            }
+            else if(dividend==-2147483648 && divisor < 0)
+            {
+                return (dividend+1)/divisor;
+            }
+            else if(dividend==-2147483647 && divisor > 0)
+            {
+                return (dividend+1)/divisor;
+            }
+
+            else
+            {
+                return dividend/divisor;
+            }
+        }
+    }
 
 
-}
+
