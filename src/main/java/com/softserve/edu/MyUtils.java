7ff4301c40d6202c6f3636d7e65348952707fc67
Rostylka <rostylka96@gmail.com>
2022-06-22 22:45:12 +0300

    Task 2.

--- src/main/java/com/softserve/edu/MyUtils.java
+++ src/main/java/com/softserve/edu/MyUtils.java
@@ -1,53 +1,34 @@
 package com.softserve.edu;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.function.BiFunction;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class MyUtils {
     public boolean verifyBrackets(String text) {
-        String openBracket = "";
+        List<String> openBracket = new LinkedList<>();
         String temp = "";
         for (int i = 0; i < text.length(); i++) {
             if (i > 1) {
                 temp = text.substring(i - 2, i);
             }
-
-            if ((text.charAt(i) == '(' || text.charAt(i) == '{' || text.charAt(i) == '[')
-                    && !temp.equals("\\\\")) {
-                openBracket = openBracket + text.charAt(i);
-                continue;
-            }
-            if (text.charAt(i) == ')' && !temp.equals("\\\\")) {
-                if (openBracket.endsWith("(") ) {
-                    openBracket = openBracket.substring(0, openBracket.length() - 1);
-                    continue;
-                } else return false;
-            }
-            if (text.charAt(i) == '}' && !temp.equals("\\\\")) {
-                if (openBracket.endsWith("{")) {
-                    openBracket = openBracket.substring(0, openBracket.length() - 1);
-                    continue;
-                } else return false;
-            }
-            if (text.charAt(i) == ']' && !temp.equals("\\\\")) {
-                if (openBracket.endsWith("[") ) {
-                    openBracket = openBracket.substring(0, openBracket.length() - 1);
-                    continue;
-                } else return false;
+            if (text.charAt(i) == '(' || text.charAt(i) == ')' || text.charAt(i) == '{'
+        || text.charAt(i) == '}' || text.charAt(i) == ']' || text.charAt(i) == '[') {
+                if (temp.equals("\\\\")) {
+                    openBracket.add(temp + text.charAt(i));
+                }
+                else {
+                    openBracket.add(text.charAt(i) + "");
+                }
             }
         }
         System.out.println(openBracket);
-        return openBracket.equals("");
+        return false;
     }
 
     public static void main(String[] args) {
         MyUtils myUtils = new MyUtils();
-        System.out.println("\\\\[\\\\{\\\\(");
         System.out.println(myUtils.verifyBrackets("\\\\[]\\\\{}\\\\()"));
     }
 }
