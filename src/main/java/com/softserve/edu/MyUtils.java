862b1adb988beb8eb282633c8d7a505cadc99261
Rostyslav <rostylka96@gmail.com>
2022-06-23 16:38:24 +0300

    Task8.6

--- src/main/java/com/softserve/edu/MyUtils.java
+++ src/main/java/com/softserve/edu/MyUtils.java
@@ -14,21 +14,146 @@ public class MyUtils {
                 temp = text.substring(i - 2, i);
             }
             if (text.charAt(i) == '(' || text.charAt(i) == ')' || text.charAt(i) == '{'
-        || text.charAt(i) == '}' || text.charAt(i) == ']' || text.charAt(i) == '[') {
+                    || text.charAt(i) == '}' || text.charAt(i) == ']' || text.charAt(i) == '[') {
                 if (temp.equals("\\\\")) {
                     openBracket.add(temp + text.charAt(i));
-                }
-                else {
+                } else {
                     openBracket.add(text.charAt(i) + "");
                 }
             }
         }
-        System.out.println(openBracket);
-        return false;
+        for (int i = 0; i < openBracket.size(); i++) {
+            if (openBracket.get(i).length() != 1) {
+                continue;
+            }
+            String temp1 = openBracket.get(i);
+            for (int j = i - 1; j >= 0; j--) {
+                String temp2 = openBracket.get(j);
+                if (temp2.length() == 1) {
+                    if (temp2.equals("{") && temp1.equals("}")) {
+                        openBracket.set(j, "");
+                        openBracket.set(i, "");
+                        break;
+                    }
+                    if (temp2.equals("[") && temp1.equals("]")) {
+                        openBracket.set(j, "");
+                        openBracket.set(i, "");
+                        break;
+                    }
+                    if (temp2.equals("(") && temp1.equals(")")) {
+                        openBracket.set(j, "");
+                        openBracket.set(i, "");
+                        break;
+                    }
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < openBracket.size(); i++) {
+            if (openBracket.get(i).length() != 1) {
+                continue;
+            }
+            String temp1 = openBracket.get(i);
+            for (int j = i - 1; j >= 0; j--) {
+                String temp2 = openBracket.get(j);
+                if (temp2.length() == 3) {
+                    if (temp2.equals("\\\\{") && temp1.equals("}")) {
+                        for (int k = i; k >= j; k--) {
+                            if (openBracket.get(k).length() != 1 || k == j)
+                                openBracket.set(k, "");
+                        }
+                        break;
+                    }
+                    if (temp2.equals("\\\\[") && temp1.equals("]")) {
+                        for (int k = i; k >= j; k--) {
+                            if (openBracket.get(k).length() != 1 || k == j)
+                                openBracket.set(k, "");
+                        }
+                        break;
+                    }
+                    if (temp2.equals("\\\\(") && temp1.equals(")")) {
+                        for (int k = i; k >= j; k--) {
+                            if (openBracket.get(k).length() != 1 || k == j)
+                                openBracket.set(k, "");
+                        }
+                        break;
+                    }
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < openBracket.size(); i++) {
+            if (openBracket.get(i).length() != 1) {
+                continue;
+            }
+            String temp1 = openBracket.get(i);
+            for (int j = i + 1; j < openBracket.size(); j++) {
+                String temp2 = openBracket.get(j);
+                if (temp2.length() == 3) {
+                    if (temp2.equals("\\\\}") && temp1.equals("{")) {
+                        for (int k = j; k >= i; k--) {
+                            if (openBracket.get(k).length() != 1 || k == i)
+                                openBracket.set(k, "");
+                        }
+                        break;
+                    }
+                    if (temp2.equals("\\\\]") && temp1.equals("[")) {
+                        for (int k = j; k >= i; k--) {
+                            if (openBracket.get(k).length() != 1 || k == i)
+                                openBracket.set(k, "");
+                        }
+                        break;
+                    }
+                    if (temp2.equals("\\\\)") && temp1.equals("(")) {
+                        for (int k = j; k >= i; k--) {
+                            if (openBracket.get(k).length() != 1 || k == i)
+                                openBracket.set(k, "");
+                        }
+                        break;
+                    }
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < openBracket.size(); i++) {
+            if (openBracket.get(i).length() != 1) {
+                continue;
+            }
+            String temp1 = openBracket.get(i);
+            for (int j = i - 1; j >= 0; j--) {
+                String temp2 = openBracket.get(j);
+                if (temp2.length() == 1) {
+                    if (temp2.equals("{") && temp1.equals("}")) {
+                        openBracket.set(j, "");
+                        openBracket.set(i, "");
+                        break;
+                    }
+                    if (temp2.equals("[") && temp1.equals("]")) {
+                        openBracket.set(j, "");
+                        openBracket.set(i, "");
+                        break;
+                    }
+                    if (temp2.equals("(") && temp1.equals(")")) {
+                        openBracket.set(j, "");
+                        openBracket.set(i, "");
+                        break;
+                    }
+                    break;
+                }
+            }
+        }
+        System.out.println(openBracket.toString());
+        boolean result = true;
+        for (String str : openBracket) {
+            if (str.length() == 1) {
+                result = false;
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         MyUtils myUtils = new MyUtils();
-        System.out.println(myUtils.verifyBrackets("\\\\[]\\\\{}\\\\()"));
+        System.out.println(myUtils.verifyBrackets("\\\\[]"));
     }
 }
