5c352d17e7e59b5cd77d800939c32bf7843745a5
Jaroslav <garasym@gmail.com>
2022-06-27 10:49:20 +0300

    JUnit4. Examples

--- src/test/java/com/softserve/edu/My.java
+++ src/test/java/com/softserve/edu/My.java
@@ -8,8 +8,33 @@ public class My {
     @Test
     public void testMy() {
         // From Maven
-        System.out.println("My done");
+        System.out.println("My testMy1()");
         //
         Assert.assertEquals(9, 4 + 5);
+    }
+
+    //@Test
+    public void testMy2() {
+        System.out.println("My testMy2()");
+        //
+        int i = 0;
+        i = 10 / i;
+        System.out.println(" i = " + i);
+    }
+
+    @Test
+    public void testMy3() {
+        System.out.println("My testMy3()");
+        //
+        double i = 0;
+        i = 10 / i;
+        System.out.println(" i = " + i);
+    }
+
+    //@Test
+    public void testMy4() {
+        System.out.println("My testMy3()");
+        //
+        throw new RuntimeException("ha-ha-ha");
     }
 }
