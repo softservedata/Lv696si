2dc0e7176f471560e170d2fec6bfc63ab33aec98
Rostyslav <rostylka96@gmail.com>
2022-07-01 17:45:49 +0300

    Task 9.5

--- src/main/java/com/softserve/edu/MyUtils.java
+++ src/main/java/com/softserve/edu/MyUtils.java
@@ -7,6 +7,18 @@ import java.util.regex.Pattern;
 
 public class MyUtils {
     public String reformatLines(String text) {
+        text = formatTextToOneLine(text);
+        StringBuilder result = formatText(text);
+        String result1 = result.toString();
+        return result.toString().trim();
+    }
+
+    /**
+     *
+     * @param text
+     * @return Text formated to One Line
+     */
+    public String formatTextToOneLine(String text) {
         while (text.startsWith("\t") ||
                 text.startsWith("\n") ||
                 text.startsWith("\\x0B") ||
@@ -14,11 +26,20 @@ public class MyUtils {
                 text.startsWith("\r")) {
             text = text.substring(1);
         }
-        text = text.replaceAll("[\\t\\n\\x0B\\f\\r]", " ");
-        text = text.replaceAll("[ ]{2,}", " ");
+        text = text.replaceAll("[\\s]+", " ");
         if (!text.endsWith(" ")) {
             text = text + " ";
         }
+        return text;
+    }
+
+    /**
+     *
+     * @param text
+     * @return reformat lines of input text.
+     * Length of every lines not exceed 60 characters.
+     */
+    public StringBuilder formatText(String text) {
         String pattern = " ";
         Pattern p = Pattern.compile(pattern);
         Matcher m = p.matcher(text);
@@ -33,20 +54,22 @@ public class MyUtils {
             previous = m.start();
         }
         result = new StringBuilder(result.substring(0, result.length() - 1));
-        String result1 = result.toString();
-        return result.toString();
+        return result;
     }
 
 
     public static void main(String[] args) {
         MyUtils myUtils = new MyUtils();
-        String text1 = " Java    was      originally developed\n" +
-                "   by    James   Gosling at Sun Microsystems (which\n" +
+        String text1 = " \t\n\tJava    was      originally developed\n,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,," +
+                "   by    James   Gosling\tat Sun Microsystems (which\n" +
                 " has since been\n" +
                 "acquired by Oracle) and released in 1995\n" +
                 " as a core component of Sun Microsystems' Java platform.    ";
         System.out.println(text1);
+        System.out.println("____________________________________________________");
         System.out.println(myUtils.reformatLines(text1));
 
     }
+
 }
+
