7512f26903c5ebb91c90e495e857b9e344859637
Rostyslav <rostylka96@gmail.com>
2022-07-05 16:42:30 +0300

    Task 9.5

---
+++ src/main/java/com/softserve/edu/PrintStackTraceDemo.java
@@ -0,0 +1,35 @@
+package com.softserve.edu;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.ByteArrayOutputStream;
+
+public class PrintStackTraceDemo{
+    public static void x(){
+        try{
+            t();
+        }
+        catch (IOException ioe){ //this way of stack trace output is workaround for moodle
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(byteArrayOutputStream);
+            ioe.printStackTrace(ps);
+            System.out.println(byteArrayOutputStream);
+        }
+    }
+
+    public static void t() throws IOException{
+        throw new IOException();
+    }
+
+    static void a() throws IOException   {
+        throw new IOException();
+    }
+
+    static void m() throws IOException   {
+        throw new IOException();
+    }
+
+    public static void main(String[] args) {
+        x();
+    }
+}
