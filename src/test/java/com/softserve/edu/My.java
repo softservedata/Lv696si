e8759ce299e216a5ca258d94da696597fb57f261
bebebe <hahaha11112@gmail.com>
2022-07-05 14:11:17 +0300

    SLF4J. Log4J. Examples

--- src/test/java/com/softserve/edu/My.java
+++ src/test/java/com/softserve/edu/My.java
@@ -1,46 +1,93 @@
 package com.softserve.edu;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class My {
+    public static final Logger logger = LoggerFactory.getLogger(My.class);
+    private String testName;
+    private boolean isOk;
+
+    @After
+    public void tearDown() throws Exception {
+        //System.out.println("\t@After tearDown()");
+        logger.info(testName + " DONE");
+        if (!isOk) {
+            logger.info("\t***" + testName + " FAILED ***");
+        }
+    }
 
     @Test
     public void testMy() {
+        isOk = false;
+        testName = "My testMy()";
+        logger.info(testName + " START");
+        //
         // From Maven
         System.out.println("My testMy1()");
         //
         Assert.assertEquals(9, 4 + 5);
+        isOk = true;
     }
 
-    //@Test
+    @Test
     public void testMy2() {
+        isOk = false;
+        testName = "My testMy2()";
+        logger.info(testName + " START");
+        //
         System.out.println("My testMy2()");
         //
         int i = 0;
         i = 10 / i;
         System.out.println(" i = " + i);
+        isOk = true;
     }
 
     @Test
     public void testMy3() {
+        isOk = false;
+        testName = "My testMy3()";
+        logger.info(testName + " START");
+        //
         System.out.println("My testMy3()");
         //
         double i = 0;
         i = 10 / i;
         System.out.println(" i = " + i);
+        isOk = true;
     }
 
-    //@Test
+    @Test
     public void testMy4() {
+        isOk = false;
+        testName = "My testMy4()";
+        logger.info(testName + " START");
+        //
         System.out.println("My testMy3()");
         //
-        throw new RuntimeException("ha-ha-ha");
+        if (!isOk) {
+            throw new RuntimeException("ha-ha-ha");
+        }
+        isOk = true;
     }
 
     @Test(expected = ArithmeticException.class)//(expectedExceptions = ArithmeticException.class)
     public void divisionWithException() {
+        isOk = false;
+        testName = "My divisionWithException()";
+        logger.info(testName + " START");
+        //
+//        if (!isOk) {
+//            throw new RuntimeException("ha-ha-ha");
+//        }
+        //
         System.out.println("\t\t\t@Test divisionWithException()");
+        isOk = true;
         int i = 1 / 0;
     }
 
