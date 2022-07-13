c57771551e3e05b9deae2e2fa0146c7b486504ff
Jaroslav <garasym@gmail.com>
2022-07-13 13:20:34 +0300

    Sprint 07 Task 01

---
+++ src/main/java/com/softserve/sprint07/task01/Pizza.java
@@ -0,0 +1,76 @@
+package com.softserve.sprint07.task01;
+
+class Pizza {
+    public static class PizzaBuilder {
+        private Pizza pizza;
+        private PizzaBuilder() {
+            pizza = new Pizza();
+        }
+
+        public PizzaBuilder addCheese(String cheese) {
+            pizza.cheese = cheese;
+            return this;
+        }
+        public PizzaBuilder addMeat(String meat) {
+            pizza.meat = meat;
+            return this;
+        }
+        public PizzaBuilder addSeafood(String seafood) {
+            pizza.seafood = seafood;
+            return this;
+        }
+        public PizzaBuilder addVegetable(String vegetable) {
+            pizza.vegetable = vegetable;
+            return this;
+        }
+        public PizzaBuilder addMushroom(String mushroom) {
+            pizza.mushroom = mushroom;
+            return this;
+        }
+        public Pizza build() {
+            return pizza;
+        }
+    }
+
+    // ---------- ---------- ---------- ---------- ---------- ---------- ---------- ----------
+    private String cheese;
+    private String meat;
+    private String seafood;
+    private String vegetable;
+    private String mushroom;
+
+    private Pizza() { }
+
+    public static PizzaBuilder base() {
+        return new PizzaBuilder();
+    }
+
+    public String getCheese() {
+        return cheese;
+    }
+    public String getMeat() {
+        return meat;
+    }
+    public String getSeafood() {
+        return seafood;
+    }
+    public String getVegetable() {
+        return vegetable;
+    }
+    public String getMushroom() {
+        return mushroom;
+    }
+}
+
+class Oven {
+    public static Pizza cook() {
+        // Create and return instance of Pizza class here
+        return Pizza.base()
+                .addCheese("Cheese")
+                .addMeat("Meat")
+                .addSeafood("Seafood")
+                .addVegetable("Vegetable")
+                .addMushroom("Mushroom")
+                .build();
+    }
+}
