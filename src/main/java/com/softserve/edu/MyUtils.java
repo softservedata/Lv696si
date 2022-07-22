077e7af79572537b1670335d7cd8df0b0e0b8a39
Rostyslav <rostylka96@gmail.com>
2022-07-22 17:27:58 +0300

    Task 14

--- src/main/java/com/softserve/edu/MyUtils.java
+++ src/main/java/com/softserve/edu/MyUtils.java
@@ -1,20 +1,48 @@
 package com.softserve.edu;
 
 
-import java.util.Map;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MyUtils {
     public Stream<String> nameList(Map<String, Stream<String>> map) {
-        Stream<String> result = null;
+        List<String> result = new ArrayList<>();
         for (Map.Entry<String, Stream<String>> entry : map.entrySet()) {
+            if (entry == null) {
+                continue;
+            }
+            List<String> temp = new ArrayList<>();
+            temp = entry.getValue().collect(Collectors.toList());
+            for (String name: temp) {
+                name = name.replaceAll(" ", "").toLowerCase();
+                if (name == null || name.isEmpty()) {
+                    continue;
+                }
 
+                name = name.substring(0, 1).toUpperCase() + name.substring(1);
+                result.add(name);
+            }
         }
-        return result;
-        // Code
+        return result.stream().distinct().sorted(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
     }
 
     public static void main(String[] args) {
-        System.out.println();
+        MyUtils myUtils = new MyUtils();
+        List<String> list1 = Arrays.asList(" iVan", "PeTro ", " Ira ");
+        List<String> list2 = Arrays.asList("STepan", "ira ", " Andriy ", "an na");
+        List<String> list3 = Arrays.asList("Ivan", "Anna");
+        Map<String , Stream<String>> map = new HashMap<>();
+        map.put("1", list1.stream());
+        map.put("2", list2.stream());
+        map.put("3", list3.stream());
+        Stream<String> str = myUtils.nameList(map);
+        str.forEach(System.out::println);
     }
 }
